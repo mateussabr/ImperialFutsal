9cce52975dff0a67634587d5dd7ca575461b941c
Mateus Abreu <abreufagundesmateus@gmail.com>
2025-07-17 17:37:56 -0300

    Corrige layout e adiciona meta viewport

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,15 @@
 import './styles/globals.css'
 import { NavBar } from './_components/NavBar';
 import { Footer } from './_components/Footer';
-import Head from 'next/head';
+
+export const metadata = {
+  title: 'Imperial Futsal',
+  description: 'Site oficial do Imperial Futsal',
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <Head>
-        <title>Imperial Futsal</title>
-        <meta name="description" content="Site oficial do Imperial Futsal" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="icon" href="/logo_time.ico" />
-      </Head>
+    <html lang="pt-BR">
       <body>
         <NavBar />
         <main>
